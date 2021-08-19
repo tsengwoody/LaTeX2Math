1c1872fbd29d9679608e24dae2931fbd65b5d7df
Tseng Woody <tsengwoody.tw@gmail.com>
2021-08-19 16:21:04 +0800

    LaTeX2Math v1.0

---
+++ precache-manifest.8980fc8e9c8d236f37798f7b87821dc8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baa4974970d6757ef468",
+    "url": "/app/LaTeX2Math/css/app.492941c4.css"
+  },
+  {
+    "revision": "926bb97fbc88ac173d95",
+    "url": "/app/LaTeX2Math/css/chunk-1bac9884.31d7fe87.css"
+  },
+  {
+    "revision": "c6a952116e5810e2dbe920a2723d5f5f",
+    "url": "/app/LaTeX2Math/index.html"
+  },
+  {
+    "revision": "baa4974970d6757ef468",
+    "url": "/app/LaTeX2Math/js/app.f54100b0.js"
+  },
+  {
+    "revision": "926bb97fbc88ac173d95",
+    "url": "/app/LaTeX2Math/js/chunk-1bac9884.9660a444.js"
+  },
+  {
+    "revision": "b30726b744f69353eaba",
+    "url": "/app/LaTeX2Math/js/chunk-vendors.3dda8145.js"
+  },
+  {
+    "revision": "bd9bde2099bbb50edcf8e46490e5fb17",
+    "url": "/app/LaTeX2Math/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/app/LaTeX2Math/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/app/LaTeX2Math/precache-manifest.8980fc8e9c8d236f37798f7b87821dc8.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "latex2math"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
